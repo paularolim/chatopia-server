1f35088b666b68492fb52875c3698a6e2a49708f
Paula Rolim <rolim.paula1999@gmail.com>
2023-05-29 21:25:17 -0300

    feat: bcypt adapter

---
+++ src/infra/criptography/bcrypt/bcypt-adapter.ts
@@ -0,0 +1,10 @@
+import bcrypt from 'bcrypt';
+import { Hasher } from '../../../data/protocols/criptography';
+
+export class BcryptAdapter implements Hasher {
+  constructor(private readonly salt: number) {}
+
+  hash(value: string): Promise<string> {
+    return bcrypt.hash(value, this.salt);
+  }
+}
