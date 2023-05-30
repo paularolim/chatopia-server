a313f81846fd371ec9d49d6cfd201a46b2c009f8
Paula Rolim <rolim.paula1999@gmail.com>
2023-05-29 21:55:20 -0300

    refactor: e-mail validation

---
+++ src/presentation/helpers/validators/email-validation.ts
@@ -0,0 +1,17 @@
+import { InvalidParamError } from '../../errors';
+import { EmailValidator, Validation } from '../../protocols';
+
+export class EmailValidation implements Validation {
+  constructor(
+    private readonly fieldName: string,
+    private readonly emailValidator: EmailValidator,
+  ) {}
+
+  validate(input: any): Error | null {
+    const isValid = this.emailValidator.isValid(input?.[this.fieldName]);
+    if (!isValid) {
+      return new InvalidParamError(this.fieldName);
+    }
+    return null;
+  }
+}
