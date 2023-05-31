c8568ac527fbf4a1a842601615d8c156990cc3b0
Paula Rolim <rolim.paula1999@gmail.com>
2023-05-31 12:32:16 -0300

    feat: validate email on login

--- src/main/factories/login/login-validation.ts
+++ src/main/factories/login/login-validation.ts
@@ -2,7 +2,9 @@ import {
   RequiredFieldsValidation,
   ValidationComposite,
 } from '../../../presentation/helpers/validators';
+import { EmailValidation } from '../../../presentation/helpers/validators/email-validation';
 import { Validation } from '../../../presentation/protocols';
+import { EmailValidatorAdapter } from '../../adapters/validators/email-validator-adapter';
 
 export const makeLoginValidation = (): Validation => {
   const validations: Validation[] = [];
@@ -10,6 +12,8 @@ export const makeLoginValidation = (): Validation => {
   for (const field of ['email', 'password']) {
     validations.push(new RequiredFieldsValidation(field));
   }
+
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()));
 
   return new ValidationComposite(validations);
 };
